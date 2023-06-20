98cc0b9f7bfe28f8055dd16fed07bd67ad8f934d
itsemmax <imanechaoub04@gmail.com>
2023-06-20 10:03:25 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,25 @@
-#include<unistd.h>
-#include "main.h"
-int main(void){
-	char *str = "_putchar";
-	int i = 0;
-	while(str[i]){
-		_putchar(str[i]);
-		i++;
-	      }
-	_putchar('\n');
-	      
-	return 0;
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function is the entry point of the program. It prints
+ *              the characters of the string "_putchar" using the _putchar
+ *              function and then prints a newline character.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char *str = "_putchar";  /* String to be printed */
+    int i = 0;              /* Loop counter */
+
+    while (str[i])
+    {
+        _putchar(str[i]);   /* Print each character of the string */
+        i++;
+    }
+
+    _putchar('\n');         /* Print a newline character */
+
+    return (0);
 }
+
