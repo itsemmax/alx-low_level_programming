520ae824207cf31c1ffc2acbe345b62fba35b193
itsemmax <imanechaoub04@gmail.com>
2023-06-20 14:28:48 -0700

    vx

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,17 @@
 #include<unistd.h>
 #include"main.h"
 /**
- * int print_sign - prints the sign of a number
+ * print_sign - Prints the sign of a number
  *
- * Returns 1 and prints + if n is greater than zero
- * Returns 0 and prints 0 if n is zero
- * Returns -1 and prints - if n is less than zero
+ * Description: This function takes an integer as input and prints the sign of
+ *              the number. If the number is greater than zero, it returns 1
+ *              and prints a plus sign (+). If the number is zero, it returns 0
+ *              and prints a zero digit (0). If the number is less than zero, it
+ *              returns -1 and prints a minus sign (-).
+ *
+ * @n: The number to check the sign of
+ *
+ * Return: 1 if @n is greater than zero, 0 if @n is zero, -1 if @n is less than zero
  */
 int print_sign(int n)
 {
@@ -27,6 +33,3 @@ _putchar('0');
 return (0);
 }
 }
-
-
-
