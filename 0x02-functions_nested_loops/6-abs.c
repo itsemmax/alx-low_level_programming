fa095628b72c5aabd7d2c89878cff92fd8853fd9
itsemmax <imanechaoub04@gmail.com>
2023-06-20 15:43:53 -0700

    v

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,10 +2,12 @@
 /**
  * _abs - Computes the absolute value of an integer
  *
- * Description: This function takes an integer as input and returns its absolute
- *              value. If the number is negative (less than zero), it returns
- *              the negation of the number (positive value). If the number is
- *              zero or positive, it returns the number as is.
+ * Description: This function takes an integer as input 
+ *              and returns its absolute value.
+ *              If the number is negative
+ *              it return the negation of the number
+ *		If the number is zero or positive,
+ *		it returns the number as is.
  *
  * @n: The number to compute the absolute value of
  *
