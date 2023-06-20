8d1360a06c2563f9cbb25ba3cea6d37520ad1a1b
itsemmax <imanechaoub04@gmail.com>
2023-06-20 12:40:33 -0700

    hk

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,25 @@
 #include<unistd.h>
 #include"main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
+ *
+ * Description: This function prints the alphabet in lowercase 10 times. It
+ *              achieves this by using nested loops. The outer loop iterates
+ *              10 times to control the number of repetitions. The inner loop
+ *              iterates from 'a' to 'z' and uses the _putchar function to
+ *              output each character. After printing the alphabet for each
+ *              repetition, it prints a newline character.
+ *
+ * Return: Nothing (void)
+ */
 void print_alphabet_x10(void)
 {
-char a ;
-int i ;
+char a;
+int i;
 
-for(i = 0;i <= 10;i++)
+for (i = 0; i < 10; i++)
 {
-for(a = 'a';a <= 'z';a++)
+for (a = 'a'; a <= 'z'; a++)
 {
 _putchar(a);
 
