80f6930dd9eb5c4b1e2523124c319c9273ecf202
itsemmax <imanechaoub04@gmail.com>
2023-06-20 13:25:51 -0700

    bn

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,27 @@
 #include<unistd.h>
 #include"main.h"
+/**
+ * _islower - Checks if a character is lowercase
+ *
+ * Description: This function checks whether a given character is lowercase
+ *              or not. It takes a single character as input and compares it
+ *              to the lowercase range of characters ('a' to 'z'). If the
+ *              character is lowercase, it returns 1. Otherwise, it returns 0.
+ *
+ * @c: The character to be checked
+ *
+ * Return: 1 if @c is lowercase, 0 otherwise
+ */
 int _islower(int c)
 {
 if (c < 'z' && c > 'a')
 {
 
-return 1;
+return (1);
 }
-else 
+else
 {
 
-return 0;
+return (0);
 }
 }
