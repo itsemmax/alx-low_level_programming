eb5de5619bd2488fc846b13b0240cbe74ca4c41f
itsemmax <imanechaoub04@gmail.com>
2023-06-26 10:03:50 -0700

    hg

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - update the value
+ * @n: pointer to an int
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef emma
+#define emma
+void reset_to_98(int *n);
+#endif
